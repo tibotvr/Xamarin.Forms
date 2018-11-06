38eef5d53f1092b023afe6665fcc0981b12fbc3e
Stephane Delcroix <stephane@delcroix.org>
2018-11-06 10:15:48 +0100

    fix merge

--- Xamarin.Forms.Core/BindableObject.cs
+++ Xamarin.Forms.Core/BindableObject.cs
@@ -439,8 +439,9 @@ namespace Xamarin.Forms
 
 		internal void ApplyBindings(bool skipBindingContext, bool fromBindingContextChanged)
 		{
-			for (int i = 0, propLength = _properties.Count; i < propLength; i++) {
-				BindablePropertyContext context = _properties [i];
+			var prop = _properties.Values.ToArray();
+			for (int i = 0, propLength = prop.Length; i < propLength; i++) {
+				BindablePropertyContext context = prop [i];
 				BindingBase binding = context.Binding;
 				if (binding == null)
 					continue;
