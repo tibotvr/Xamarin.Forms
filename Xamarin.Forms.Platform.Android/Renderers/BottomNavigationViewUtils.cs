d469f9eb2ee551e57d70bfcfd68d727a053b5417
ShaneN <shane94@hotmail.com>
2018-11-16 10:18:55 -0700

    support v28 and android 9.0 updates

--- Xamarin.Forms.Platform.Android/Renderers/BottomNavigationViewUtils.cs
+++ Xamarin.Forms.Platform.Android/Renderers/BottomNavigationViewUtils.cs
@@ -12,6 +12,11 @@ using Android.Widget;
 using Android.Support.Design.Widget;
 using Android.Support.Design.Internal;
 
+#if __ANDROID81__
+#else
+using ALabelVisibilityMode = Android.Support.Design.BottomNavigation.LabelVisibilityMode;
+#endif
+
 namespace Xamarin.Forms.Platform.Android
 {
 	public static class BottomNavigationViewUtils
@@ -28,27 +33,35 @@ namespace Xamarin.Forms.Platform.Android
 						return;
 					}
 
-
+#if __ANDROID81__
 					var shiftMode = menuView.Class.GetDeclaredField("mShiftingMode");
-
 					shiftMode.Accessible = true;
 					shiftMode.SetBoolean(menuView, enableShiftMode);
 					shiftMode.Accessible = false;
 					shiftMode.Dispose();
-
-
+#else
+					if (enableShiftMode)
+						bottomNavigationView.LabelVisibilityMode = ALabelVisibilityMode.LabelVisibilityAuto;
+					else
+						bottomNavigationView.LabelVisibilityMode = ALabelVisibilityMode.LabelVisibilityLabeled;
+#endif
 					for (int i = 0; i < menuView.ChildCount; i++)
 					{
 						var child = menuView.GetChildAt(i);
 						var item = child as BottomNavigationItemView;
 						if (item != null)
 						{
+#if __ANDROID81__
 							item.SetShiftingMode(enableItemShiftMode);
+#else
+							item.SetShifting(enableItemShiftMode);
+#endif
 							item.SetChecked(item.ItemData.IsChecked);
 						}
 
 						child.Dispose();
 					}
+
 
 					menuView.UpdateMenuView();
 				}
