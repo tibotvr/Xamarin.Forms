462497c33ec1d10c9b87609df4bdd9da40b0a05a
E.Z. Hart <hartez@gmail.com>
2018-12-05 14:00:08 -0700

    Implement Android single selection

--- Xamarin.Forms.Platform.Android/CollectionView/CarouselViewRenderer.cs
+++ Xamarin.Forms.Platform.Android/CollectionView/CarouselViewRenderer.cs
@@ -23,7 +23,7 @@ namespace Xamarin.Forms.Platform.Android
 			// But for the Carousel, we want it to create the items to fit the width/height of the viewport
 			// So we give it an alternate delegate for creating the views
 
-			ItemsViewAdapter = new ItemsViewAdapter(ItemsView, Context, 
+			ItemsViewAdapter = new ItemsViewAdapter(ItemsView, 
 				(renderer, context) => new SizedItemContentControl(renderer, context, () => Width, () => Height));
 
 			SwapAdapter(ItemsViewAdapter, false);
